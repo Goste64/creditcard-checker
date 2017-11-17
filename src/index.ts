62df90cb968fe0322b92a9ff4217dd6a532f3ffc
goste_ <aidanhamilton@live.com>
2017-11-17 14:43:01 +1000

    line reader working in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+// Read numbers from a file
+// check companys, validate
+// output numbers with companys + validation status
+
+const lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('data/input.txt')
+})
+
+lineReader.on('line', function(line: string) {
+  console.log('Line from file:', line)
+})
