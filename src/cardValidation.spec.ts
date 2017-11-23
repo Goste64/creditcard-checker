ad75036f205c7021d9dc60ebc794f5e3befd3d94
goste_ <aidanhamilton@live.com>
2017-11-23 15:57:59 +1000

    updated unit testing

--- src/cardValidation.spec.ts
+++ src/cardValidation.spec.ts
@@ -2,10 +2,18 @@ import cardValidator from './cardValidator'
 
 describe('cardValidator', () => {
   test('Returns true if given a valid number', () => {
-    expect(cardValidator('1234567812345670')).toBe(true)
+    expect(cardValidator('4012888888881881')).toBe(true)
+    expect(cardValidator('4111111111111111')).toBe(true)
+    expect(cardValidator('6011111111111117')).toBe(true)
+    expect(cardValidator('5105105105105100')).toBe(true)
   })
 
   test('returns false if given an invalid number', () => {
-    expect(cardValidator('00000000000444')).toBe(false)
+    expect(cardValidator('4111111111111')).toBe(false)
+    expect(cardValidator('378282246310005')).toBe(false)
+    expect(cardValidator('5105105105105106')).toBe(false)
+    expect(cardValidator('9111111111111111')).toBe(false)
+    expect(cardValidator('371449635398431')).toBe(false)
+    expect(cardValidator('378734493671000')).toBe(false)
   })
 })
