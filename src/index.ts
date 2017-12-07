2b67431c3b5d2d54550bcb29963d16c2a1da671c
goste_ <aidanhamilton@live.com>
2017-12-07 15:37:59 +1000

    done finished working

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 // Read numbers from a file *TICK*
 // check companys, validate *TICK*
-// output numbers with companys + validation status
+// output numbers with companys + validation status *TICK && failure*
 import { createReadStream } from 'fs'
 import { createInterface } from 'readline'
 import { getCompanyName } from './cardType'
 import cardValidator from './cardValidator'
+import * as fs from 'fs'
 
 const lineReader = createInterface({
   input: createReadStream('data/input.txt')
 })
 
-lineReader.on('line', (creditCard: string) => {
-  var company = `${getCompanyName(creditCard)}: ${creditCard} (${
-    cardValidator(creditCard) ? 'valid' : 'invalid'
-  })`
+const output: string[] = []
 
-  console.log(company)
+lineReader.on('line', (creditCard: string) => {
+  output.push(
+    `${getCompanyName(creditCard)}: ${creditCard} (${
+      cardValidator(creditCard) ? 'valid' : 'invalid'
+    })`
+  )
 })
+
+lineReader.on('close', () => {
+  fs.writeFile('./data/output.txt', output.join('\n'), err => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
+})
+//make company output an array
